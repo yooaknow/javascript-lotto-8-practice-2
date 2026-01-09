dde8ab15825310942b1a12e8f9b89e099bd1bf06
yooaknow <dbdkfma1228@naver.com>
2026-01-09 16:12:16 +0900

    feat: App.js에서 입력값, 출력값, 컨트롤러 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import InputView from "./View/InputView.js";
+import OutputView from "./View/OutputView.js";
+import AppController from "./Controller/AppController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const inputView = new InputView();
+    const outputView = new OutputView();
+
+    const controller = new AppController(inputView, outputView);
+    await controller.run();
+  }
 }
 
 export default App;
